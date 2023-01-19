595b5f0fd48e27e35cb265e5eaf91d43b70e1a4e
yejin0604 <104920062+hovg33399@users.noreply.github.com>
2023-01-19 21:56:36 +0900

    Add files via upload

---
+++ movieDummy.js
@@ -0,0 +1,394 @@
+export const dummy = {
+  "dates": {
+  "maximum": "2023-01-24",
+  "minimum": "2022-12-07"
+  },
+  "page": 1,
+  "results": [
+  {
+  "adult": false,
+  "backdrop_path": "/r9PkFnRUIthgBp2JZZzD380MWZy.jpg",
+  "genre_ids": [
+  16,
+  28,
+  12,
+  35,
+  10751,
+  14
+  ],
+  "id": 315162,
+  "original_language": "en",
+  "original_title": "Puss in Boots: The Last Wish",
+  "overview": "아홉 개의 목숨 중 단 하나의 목숨만 남은 장화신은 고양이.  마지막 남은 목숨을 지키기 위해 히어로의 삶 대신 반려묘의 삶을 선택한 그에게 찾아온 마지막 기회, 바로 소원을 들어주는 소원별이 있는 곳을 알려주는 지도!  잃어버린 목숨을 되찾고 다시 히어로가 되기를 꿈꾸는 장화신은 고양이는 뜻밖에 동료가 된 앙숙 파트너 '키티 말랑손', 그저 친구들과 함께라면 모든 게 행복한 강아지 '페로'와 함께 소원별을 찾기 위해 길을 떠난다.  그리고 소원별을 노리는 또 다른 빌런들과 마주치게 되는데…",
+  "popularity": 9694.931,
+  "poster_path": "/rKgvctIuPXyuqOzCQ16VGdnHxKx.jpg",
+  "release_date": "2023-01-04",
+  "title": "장화신은 고양이: 끝내주는 모험",
+  "video": false,
+  "vote_average": 8.6,
+  "vote_count": 1981
+  },
+  {
+  "adult": false,
+  "backdrop_path": "/s16H6tpK2utvwDtzZ8Qy4qm5Emw.jpg",
+  "genre_ids": [
+  878,
+  12,
+  28
+  ],
+  "id": 76600,
+  "original_language": "en",
+  "original_title": "Avatar: The Way of Water",
+  "overview": "판도라 행성에서 제이크 설리와 네이티리가 이룬 가족이 겪게 되는 무자비한 위협과 살아남기 위해 떠나야 하는 긴 여정과 전투, 그리고 견뎌내야 할 상처에 대한 이야기를 그렸다. 살아남기 위해 설리 가족이 숲에서 바다로 터전을 옮기면서 겪게 되는 화합의 과정, 그리고 곳곳에서 도사리는 새로운 위협까지 역경 속에서 더 아름답게 펼쳐진다.",
+  "popularity": 2736.974,
+  "poster_path": "/z56bVX93oRG6uDeMACR7cXCnAbh.jpg",
+  "release_date": "2022-12-14",
+  "title": "아바타: 물의 길",
+  "video": false,
+  "vote_average": 7.7,
+  "vote_count": 4330
+  },
+  {
+  "adult": false,
+  "backdrop_path": "/7dm64SW5L5CCg47kAEAcdCGaq5i.jpg",
+  "genre_ids": [
+  27,
+  53
+  ],
+  "id": 676547,
+  "original_language": "en",
+  "original_title": "Prey for the Devil",
+  "overview": "과거의 트라우마로 여전히 고통 받고 있는 수녀 앤  구마 의식 학교에서 특별한 재능을 인정받으며 남성 사제에게만 허용되었던 구마 의식 훈련을 받게 되고  그곳에서 만난 환자들을 통해 어릴 적부터 그녀를 괴롭혀 왔던 존재의 실체와 만나게 되는데...",
+  "popularity": 1047.115,
+  "poster_path": "/zfWbFiCpi9rlr96hTQpNoqiSKr7.jpg",
+  "release_date": "2022-12-07",
+  "title": "프레이 포 더 데블",
+  "video": false,
+  "vote_average": 7.2,
+  "vote_count": 393
+  },
+  {
+  "adult": false,
+  "backdrop_path": "/c1bz69r0v065TGFA5nqBiKzPDys.jpg",
+  "genre_ids": [
+  35,
+  10751,
+  10402
+  ],
+  "id": 830784,
+  "original_language": "en",
+  "original_title": "Lyle, Lyle, Crocodile",
+  "overview": "쇼맨 헥터(하비에르 바르뎀)는 어느 날 노래하는 악어 라일(숀 멘데스)을 발견하게 되고, 돈과 명예를 얻게 될 성공적인 무대를 꿈꾼다. 그러나 무대공포증에 있던 라일로 인해 무대는 실패하고 헥터는 라일을 홀로 도심에 남기고 떠난다.​​  한편, 뉴욕에 이사 온 프림 가족은 매일 밤 의문의 노랫소리로 신기한 경험을 하게 되고, 그 놀라운 정체와 마주하게 되면서 인생이 바뀌게 되는데…",
+  "popularity": 390.994,
+  "poster_path": "/aWr7F5vK8LIY31sqQgINobBjBBK.jpg",
+  "release_date": "2023-01-18",
+  "title": "라일 라일 크로커다일",
+  "video": false,
+  "vote_average": 7.7,
+  "vote_count": 282
+  },
+  {
+  "adult": false,
+  "backdrop_path": "/mSyQoValhBsJdq3JNGXJww2Q5yL.jpg",
+  "genre_ids": [
+  35,
+  27,
+  53
+  ],
+  "id": 593643,
+  "original_language": "en",
+  "original_title": "The Menu",
+  "overview": "커플인 마고와 타일러는 까다롭기로 유명한 셰프, 슬로윅의 초청을 받아 어느 외딴섬 레스토랑의 저녁 식사에 참석하게 된다. 평소 슬로윅을 존경해왔던 타일러는 들뜬 마음으로 여러 유명인사들과 함께 섬으로 출발하는 배에 탑승한다. 레스토랑에 도착한 손님들은 슬로윅의 화려한 환대에 탄성을 자아낸다. 하지만 ‘음식은 또 다른 예술’이라는 슬로윅의 기괴한 집착에, 좋았던 분위기는 점차 이상한 방향으로 흐른다. 뭔가 심상치 않음을 감지한 마고는 이의를 제기하려 하지만, 이미 슬로윅의 음식에 푹 빠져버린 타일러에게 그녀의 제안은 안중에도 없다. 그런 와중에 슬로윅의 기괴한 집착은 점점 선을 넘기 시작하는데...",
+  "popularity": 382.481,
+  "poster_path": "/2Bi9OTGNAkNjStDnMDR93GPgwFY.jpg",
+  "release_date": "2022-12-07",
+  "title": "더 메뉴",
+  "video": false,
+  "vote_average": 7.3,
+  "vote_count": 1580
+  },
+  {
+  "adult": false,
+  "backdrop_path": "/4QB2TfxmzgMDLmsVVk1HM4tt7ef.jpg",
+  "genre_ids": [
+  35,
+  18,
+  878
+  ],
+  "id": 744594,
+  "original_language": "en",
+  "original_title": "White Noise",
+  "overview": "미국의 조용한 마을에 살고 있는 잭은 대학에서 히틀러를 연구하는 교수이다. 아내와 네 명의 자녀를 둔 그의 인생은 남들처럼 평범하기 그지없다. 어느날, 화학 폐기물을 실은 트럭이 열차와 충돌하는 사고가 발생하면서 마을 전체에 화학 폐기물이 퍼진다. 대피 명령이 내려지면서 평화로웠던 마을은 금세 아수라장이 되고, 사태의 위험성을 깨달은 잭은 가족들과 탈출 행렬에 합류하는데…",
+  "popularity": 114.839,
+  "poster_path": "/iaItCRbjr70awVPeaCRUDLPTl80.jpg",
+  "release_date": "2022-12-07",
+  "title": "화이트 노이즈",
+  "video": false,
+  "vote_average": 5.7,
+  "vote_count": 273
+  },
+  {
+  "adult": false,
+  "backdrop_path": "/zjpYDQlhvrAaohKRShu522sKJ87.jpg",
+  "genre_ids": [
+  16,
+  35,
+  18
+  ],
+  "id": 783675,
+  "original_language": "ja",
+  "original_title": "THE FIRST SLAM DUNK",
+  "overview": "전국 제패를 꿈꾸는 북산고 농구부 5인방의 꿈과 열정, 멈추지 않는 도전을 그린 영화",
+  "popularity": 78.583,
+  "poster_path": "/coiJrdXAXuBkSGDvp9bZ7mkuU6E.jpg",
+  "release_date": "2023-01-04",
+  "title": "더 퍼스트 슬램덩크",
+  "video": false,
+  "vote_average": 8.2,
+  "vote_count": 10
+  },
+  {
+  "adult": false,
+  "backdrop_path": "/qp8qKiP7Q7zK4z3LItwWMHfV9kJ.jpg",
+  "genre_ids": [
+  53
+  ],
+  "id": 764835,
+  "original_language": "en",
+  "original_title": "The Desperate Hour",
+  "overview": "긴급 경보 : 교내 총격 테러 발생  오전 8:00 에이미는 학교를 가기 싫어하는 아들을 뒤로 한 채 조깅을 나선다. 오전 8:59 아들의 학교에서 총격 테러가 발생했다. 오전 9:00 아들에게 연락이 닿지 않는다. 오전 9:36 아들이 유력한 용의자로 의심받고 있는 것을 알게 된다.  단 한 걸음도, 단 한 순간도 놓치지 못할 리얼 타임 서스펜스가 펼쳐진다!",
+  "popularity": 74.543,
+  "poster_path": "/nowGRB6389FFRoGEhq4wsu1GOit.jpg",
+  "release_date": "2023-01-04",
+  "title": "패닉 런",
+  "video": false,
+  "vote_average": 6.1,
+  "vote_count": 310
+  },
+  {
+  "adult": false,
+  "backdrop_path": "/33qGtN2GpGEb94pn25PDPeWQZLk.jpg",
+  "genre_ids": [
+  28
+  ],
+  "id": 894169,
+  "original_language": "en",
+  "original_title": "Vendetta",
+  "overview": "딸과 함께 평온한 일상을 보내고 있던 '윌리엄 던컨'.  그러나 의문의 범죄 집단에 의해 딸이 잔혹하게 살해되는 사건이 발생하고, 그녀를 죽인 범인 '대니'는 가벼운 형만을 선고받는다.  결국 자신의 손으로 직접 딸의 복수를 결심한 '던컨'은 '대니'를 살해하지만, 이는 그가 속한 범죄 집단과의 전면전을 알리는 계기가 되어 끝도 없는 전쟁의 서막이 열리는데...",
+  "popularity": 67.313,
+  "poster_path": "/uf3HYK3VdcXqH7wZVqQK58U0mO3.jpg",
+  "release_date": "2023-01-20",
+  "title": "벤데타: 복수의 시간",
+  "video": false,
+  "vote_average": 6.3,
+  "vote_count": 192
+  },
+  {
+  "adult": false,
+  "backdrop_path": "/9VIURlZvfnmFACs2m0MdUDDC3In.jpg",
+  "genre_ids": [
+  14,
+  18,
+  10749
+  ],
+  "id": 556694,
+  "original_language": "en",
+  "original_title": "Three Thousand Years of Longing",
+  "overview": "세상 모든 이야기에 통달한 서사학자 알리테아가 우연히 소원을 이뤄주는 정령 지니를 깨워낸다. 그녀에게 주어진 기회는 단 세 번. 마음속 가장 깊은 곳, 가장 오랫동안 바라온 소원을 말할 것!",
+  "popularity": 48.578,
+  "poster_path": "/hHmJGgb29sIxSK9EOf6d68mDnEM.jpg",
+  "release_date": "2023-01-04",
+  "title": "3000년의 기다림",
+  "video": false,
+  "vote_average": 7.1,
+  "vote_count": 611
+  },
+  {
+  "adult": false,
+  "backdrop_path": "/1cnknzG8hDYq6qYk75HGzFTre4m.jpg",
+  "genre_ids": [
+  80,
+  28
+  ],
+  "id": 606400,
+  "original_language": "ko",
+  "original_title": "교섭",
+  "overview": "분쟁지역 아프가니스탄에서 한국인들이 탈레반에게 납치되는 최악의 피랍사건이 발생한다. 교섭 전문이지만 아프가니스탄은 처음인 외교관 재호가 현지로 향하고, 국정원 요원 대식을 만난다. 원칙이 뚜렷한 외교관과 현지 사정에 능통한 국정원 요원. 입장도 방법도 다르지만,  사람은 인질을 살려야 한다는 목표를 향해 함께 나아간다. 살해 시한은 다가오고, 협상 상대, 조건 등이 시시각각 변하는 상황에서 교섭의 성공 가능성은 점점 희박해져 가는데...",
+  "popularity": 37.985,
+  "poster_path": "/qaVQoBuICpRq4i5Ke4Jm2ZngvxF.jpg",
+  "release_date": "2023-01-18",
+  "title": "교섭",
+  "video": false,
+  "vote_average": 0,
+  "vote_count": 0
+  },
+  {
+  "adult": false,
+  "backdrop_path": "/zcD36wMlsjxTOrL2Qv5Ma4GjHh.jpg",
+  "genre_ids": [
+  28
+  ],
+  "id": 715253,
+  "original_language": "ko",
+  "original_title": "유령",
+  "overview": "1933년, 일제강점기 경성. 항일조직 흑색단의 스파이인 유령이 비밀리에 활약하고 있다. 새로 부임한 경호대장 카이토는 흑색단의 총독 암살 시도를 막기 위해 조선총독부 내의 유령을 잡으려는 덫을 친다. 영문도 모른 채, 유령으로 의심받고 벼랑 끝 외딴 호텔에 갇힌 용의자들. 총독부 통신과 감독관 쥰지, 암호문 기록 담당 차경, 정무총감 비서 유리코, 암호 해독 담당 천계장, 통신과 직원 백호. 이들에게 주어진 시간은 단 하루 뿐. 기필코 살아나가 동지들을 구하고 총독 암살 작전을 성공시켜야 하는 유령과 무사히 집으로 돌아가고 싶은 이들 사이, 의심과 경계는 점점 짙어지는데…  과연 유령은 작전에 성공할 수 있을 것인가?  “성공할 때까지 멈춰서는 안 된다”",
+  "popularity": 32.343,
+  "poster_path": "/lztFg9XBae2N292ZMaBFdjRYjjA.jpg",
+  "release_date": "2023-01-18",
+  "title": "유령",
+  "video": false,
+  "vote_average": 0,
+  "vote_count": 0
+  },
+  {
+  "adult": false,
+  "backdrop_path": null,
+  "genre_ids": [
+  16
+  ],
+  "id": 1060404,
+  "original_language": "ko",
+  "original_title": "캐리와 슈퍼콜라",
+  "overview": "우주 악당 ‘스펙터’에게 쫓기던 외계인 ‘마스터’는  우연히 지구에 불시착하게 되고  ‘캐리’의 가장 소중한 인형 ‘콜라’의 몸속에 들어가게 된다  그렇게 처음 ‘슈퍼콜라’와 만나게 된 11살 캐리와 친구들!  자유자재로 사물을 움직일 수 있는 초능력 덕분에  ‘캐리’의 하나뿐인 비밀친구가 된 ‘슈퍼 콜라’!  그러나 평온함도 잠시. 무리한 초능력 사용으로  ‘스펙터’ 일당의 레이더에 걸린 ‘슈퍼콜라’는  결국 악당의 우주선으로 잡혀가고 마는데!  용감무쌍 캐리의 슈퍼파워 콜라 구출 대작전!",
+  "popularity": 23.193,
+  "poster_path": "/oy7vvceKkbIal3t5nGfUWFlqXOl.jpg",
+  "release_date": "2023-01-18",
+  "title": "캐리와 슈퍼콜라",
+  "video": false,
+  "vote_average": 0,
+  "vote_count": 0
+  },
+  {
+  "adult": false,
+  "backdrop_path": "/cuyXEls6uOMDqflx2MSzZOuQSn2.jpg",
+  "genre_ids": [
+  35,
+  18
+  ],
+  "id": 668640,
+  "original_language": "es",
+  "original_title": "Competencia oficial",
+  "overview": "가진 것 없이 맨손으로 시작해 자수성가한 억만장자. 세상의 모든 부귀영화를 누리지만 명성에 목마른 그는 80세 생일 기념으로, ‘훌륭한 영화’를 만들라고 지시한다. 노벨문학상 판권을 돈으로 사버리고 전 세계 영화제를 휩쓴 경력을 지닌 ‘롤라 쿠에바스’를 감독으로 기용한다. 천재 감독으로 불리지만 괴짜 기질이 있다고 알려진 ‘롤라 쿠에바스’ 감독은, 자신이 연출할 영화에 연기 거장 ‘이반 토레스’와 월드 스타 ‘펠릭스 리베로’를 캐스팅한다.  넘쳐흐르는 제작비로 최고의 원작, 최고의 감독, 최고의 배우가 뭉치고, 지상 최대의 걸작 만들기 프로젝트가 시작된다!",
+  "popularity": 17.095,
+  "poster_path": "/z5UWRwfh04cPNaKpXpvqUfIoJdq.jpg",
+  "release_date": "2022-12-28",
+  "title": "크레이지 컴페티션",
+  "video": false,
+  "vote_average": 6.8,
+  "vote_count": 229
+  },
+  {
+  "adult": false,
+  "backdrop_path": null,
+  "genre_ids": [
+  18
+  ],
+  "id": 1063134,
+  "original_language": "ko",
+  "original_title": "겨울 이야기",
+  "overview": "신상옥 감독의 유작이 된 작품이다. 아내의 죽음으로 인한 충격으로 정신을 놓은 채 치매에 걸린 노인(신구)과 그를 돌보는 며느리(김지숙), 그리고 온 가족이 겪는 고통과 갈등, 회환을 그린다. 친숙한 배우 신구와 연극계 스타 김지숙이 각각 노인과 며느리로 분해 호흡을 맞췄다. 1994년 프랑스 깐느 영화제 심사위원, 2002년 프랑스 도빌영화제 심사위원장, 뮤지컬 <누구를 위해 종은울리나> 총연출 등 영화 제작 외적으로도 활발한 활동을 펼치던 신상옥 감독은 2002년 이 작품으로 6년여 만에 영화촬영 현장으로 복귀했다. 2004년에 제작이 완료된 이 작품은 2006년 4월 신상옥 감독의 갑작스러운 별세로 인해 개봉되지 못한 채 유작으로 남았다.",
+  "popularity": 22.832,
+  "poster_path": "/8uLDhDDyTzJImKzWiJZZho2xfIl.jpg",
+  "release_date": "2023-01-18",
+  "title": "겨울 이야기",
+  "video": false,
+  "vote_average": 0,
+  "vote_count": 0
+  },
+  {
+  "adult": false,
+  "backdrop_path": "/r6aqhlmJmu8Dv5E7QYEruaEXKYm.jpg",
+  "genre_ids": [
+  18
+  ],
+  "id": 758866,
+  "original_language": "ja",
+  "original_title": "ドライブ・マイ・カー",
+  "overview": "누가 봐도 아름다운 부부 가후쿠와 오토.  우연히 아내의 외도를 목격한 가후쿠는 이유를 묻지 못한 채 갑작스럽게 아내의 죽음을 맞이하게 된다. 2년 후 히로시마의 연극제에 초청되어 작품의 연출을 하게 된 가후쿠. 그는 그곳에서 자신의 전속 드라이버 미사키를 만나게 된다. 말없이 묵묵히 가후쿠의 차를 운전하는 미사키와 오래된 습관인 아내가 녹음한 테이프를 들으며 대사를 연습하는 가후쿠. 조용한 차 안에서 두 사람은 점점 마음을 열게 되고, 서로가 과거의 아픔에서 벗어나지 못한 것을 알게 된다.  그리고 눈 덮인 홋카이도에서 내면에 깊숙이 자리 잡은 서로의 슬픔을 들여다보게 되는데….",
+  "popularity": 20.322,
+  "poster_path": "/5L1SKj1UyDE4rmRNtY1RwFdRhe7.jpg",
+  "release_date": "2021-12-23",
+  "title": "드라이브 마이 카",
+  "video": false,
+  "vote_average": 7.4,
+  "vote_count": 859
+  },
+  {
+  "adult": false,
+  "backdrop_path": "/kgP4YOUWKhJZWL0Z8LJ6G6LBMEM.jpg",
+  "genre_ids": [
+  10752,
+  18
+  ],
+  "id": 581577,
+  "original_language": "de",
+  "original_title": "Persian Lessons",
+  "overview": "페르시아어를 배우기 원하는 독일군 장교 코흐. 살기 위해 페르시아인이라고 거짓말을 한 유대인 질. 질은 살아남기 위해 코흐에게 가짜 페르시아어를 가르치고 매일 밤 거짓으로 단어를 만드는데...",
+  "popularity": 26.037,
+  "poster_path": "/bem8vujUVBBhd7ktTs90X3zzWtm.jpg",
+  "release_date": "2022-12-15",
+  "title": "페르시아어 수업",
+  "video": false,
+  "vote_average": 7.7,
+  "vote_count": 282
+  },
+  {
+  "adult": false,
+  "backdrop_path": "/9BQPM1Fa1hdXaE7hNafO5WWNv9v.jpg",
+  "genre_ids": [
+  27,
+  53
+  ],
+  "id": 631947,
+  "original_language": "en",
+  "original_title": "La Llorona",
+  "overview": "변화를 위해 특별히 멕시코로 휴가를 떠난 ‘칼리’ 가족. 하지만 그들이 머무는 곳에 밤마다 아이들이 종종 사라진다. 아이를 데려간다는 멕시코의 전설적인 귀신 요로나, 그녀의 저주가 점차 다가오기 시작하고 결국 칼리의 아들 ‘대니’가 요로나에게 납치당해 행방불명된다. 사라진 아들을 찾기 위해 ‘칼리’ 부부는 요로나에 맞서기로 하는데…  과연 그들은 요로나의 저주로부터 벗어날 수 있을 것인가?",
+  "popularity": 17.997,
+  "poster_path": "/seAK7B5ZuWhQ1h6CtWEhXsMPcTM.jpg",
+  "release_date": "2022-12-30",
+  "title": "요로나: 끝나지 않은 저주",
+  "video": false,
+  "vote_average": 4.7,
+  "vote_count": 36
+  },
+  {
+  "adult": false,
+  "backdrop_path": "/p9R163gjE6Z5g4eWY1qnvU4vbW2.jpg",
+  "genre_ids": [
+  18,
+  36
+  ],
+  "id": 888321,
+  "original_language": "de",
+  "original_title": "Corsage",
+  "overview": "유럽에서 가장 아름다운 여인으로 유명한 오스트리아의 황후 엘리자베트. 그에게 주어진 역할은 1킬로의 머리를 이고 우아하게 앉아있는 것뿐이다. 갑갑한 황실에서 벗어나고 싶은 엘리자베트는 자유를 찾아 자신을 조이던 코르사주를 벗고 스스로의 초상을 완성하려 한다.",
+  "popularity": 17.919,
+  "poster_path": "/mmWB2sfDFaMDS2N0kNHRkuQZqTK.jpg",
+  "release_date": "2022-12-21",
+  "title": "코르사주",
+  "video": false,
+  "vote_average": 6.9,
+  "vote_count": 67
+  },
+  {
+  "adult": false,
+  "backdrop_path": null,
+  "genre_ids": [
+  16
+  ],
+  "id": 1064734,
+  "original_language": "ko",
+  "original_title": "Carrie and Superkola",
+  "overview": "",
+  "popularity": 23.8,
+  "poster_path": "/zBJraSKudxb3eiDdwKX03mYi5Gx.jpg",
+  "release_date": "2023-01-19",
+  "title": "캐리와 슈퍼콜라",
+  "video": false,
+  "vote_average": 0,
+  "vote_count": 0
+  }
+  ],
+  "total_pages": 4,
+  "total_results": 64
+  }
